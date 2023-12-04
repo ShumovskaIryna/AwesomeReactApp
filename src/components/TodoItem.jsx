3477931b84b9819e2d648b4ce14cd673731cf344
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-12-05 00:17:29 +0200

    add responsive design

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { FaTrash, FaPen } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { deleteTodo, updateTodo } from "../redux/slices/todoListSlice";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const TodoItem = (props) => {
   const [isChecked, setChecked] = useState(props.todoItem.checked);
@@ -37,48 +42,63 @@ const TodoItem = (props) => {
   };
 
   return (
-    <>
-      <tr className="fw-normal">
-        <th className="align-middle">
-          <div className="checkbox">
-            <input
-              type="checkbox"
+    <Row className="d-flex justify-content-between">
+      <Col xs={1} md={1}>
+        <div
+          id={props.todoItem.id}
+          checked={isChecked}
+          onClick={() => handleCheck(props.todoItem)}
+          className={`checkbox ${isChecked ? "checked" : ""}`}
+        ></div>
+      </Col>
+      <Col xs={8} md={9}>
+        <InputGroup>
+          <Col xs={10} sm={11}>
+            <Form.Control
+              aria-describedby="basic-addon2"
+              type="text"
               id={props.todoItem.id}
-              checked={isChecked}
-              onChange={() => handleCheck(props.todoItem)}
+              value={title}
+              onChange={(e) => handleChange(e)}
+              disabled={isDisabled}
+              style={{
+                borderStyle: `${isDisabled ? "none" : "initial"}`,
+                backgroundColor: "#b9ccff57",
+                color: "black",
+              }}
             />
-            <label htmlFor={props.todoItem.id}>
-              <input
-                type="text"
-                id={props.todoItem.id}
-                value={title}
-                onChange={(e) => handleChange(e)}
-                disabled={isDisabled}
-              />
-              {!isDisabled ? (
-                <button onClick={() => handleUpdate(props.todoItem)}>
-                  Save
-                </button>
-              ) : (
-                <></>
-              )}
-            </label>
-          </div>
-        </th>
-        <td className="align-middle">
-          <FaPen
-            role="delete_button"
-            className="fas fa-trash-alt fa-lg text-info"
-            onClick={() => setDisabled((isDisabled) => !isDisabled)}
-          />
-          <FaTrash
-            role="delete_button"
-            className="fas fa-trash-alt fa-lg text-warning"
-            onClick={() => handleRemove(props.todoItem)}
-          />
-        </td>
-      </tr>
-    </>
+          </Col>
+          {!isDisabled ? (
+            <Col xs={2} sm={1}>
+              <Button
+                variant="outline-dark"
+                onClick={() => handleUpdate(props.todoItem)}
+                size="sm"
+              >
+                Save
+              </Button>
+            </Col>
+          ) : (
+            <></>
+          )}
+        </InputGroup>
+      </Col>
+
+      <Col xs={1} md={1}>
+        <FaPen
+          role="update_button"
+          className="fas fa-edit fa-lg me-2"
+          onClick={() => setDisabled((isDisabled) => !isDisabled)}
+        />
+      </Col>
+      <Col xs={1} md={1}>
+        <FaTrash
+          role="delete_button"
+          className="fas fa-trash-alt fa-lg"
+          onClick={() => handleRemove(props.todoItem)}
+        />
+      </Col>
+    </Row>
   );
 };
 export default TodoItem;
